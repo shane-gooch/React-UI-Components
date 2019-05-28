35d7e16ff4c1a4e05a0e936377f2c68aaf48b5d3
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-05-28 08:05:08 -0700

    worked on header styling, still to do more

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -5,12 +5,15 @@ import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
 import HeaderContent from './HeaderContent'
 
+
 function HeaderContainer() {
     return (
         <div className='HeaderContainer'>
-            <ImageThumbnail /> 
-            <HeaderTitle />
-            <HeaderContent />
+            <ImageThumbnail />
+            <div className='content'> 
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
         </div>
     );
 }
