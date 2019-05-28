a9d2a009755509764643f3e1e01a88d00fd9b590
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-05-27 23:08:12 -0700

    finished mvp

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+function CardContainer() {
+    return (
+        <a href='reactjs.org'>
+            <div className='CardContainer'>
+                <CardBanner />
+                <CardContent />
+            </div>
+        </a>
+    );
+}
+
+export default CardContainer; 
